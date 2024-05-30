67ba768117716491c9a6e71efb390cfee5922d1c
Nikalya_1398 <v.ninmathirasa@gmail.com>
2024-05-30 08:31:31 +0530

    added files

---
+++ Class.java
@@ -0,0 +1,34 @@
+public class Class{
+
+    public static void main(String[] args) {
+        
+      //object
+        Class obj = new Class();
+        
+        // value 
+        obj.name ="Joe";
+        obj.rollno = 1001;
+
+        //method call
+        obj. display();
+        
+    }
+
+   
+    
+        // variables
+       
+        String name;
+        int rollno;
+
+        //method
+
+       void display(){
+
+            System.out.println(name);
+            System.out.println(rollno);
+
+        }
+    
+    
+}
